a1c60897cef24e4d5d3304d2f353e51e77de8730
aialok <alokgupta1560@gmail.com>
2025-11-23 13:29:19 +0530

    Add initial implementation of booking system for fitness classes

---
+++ index.js
@@ -0,0 +1,244 @@
+"use strict";
+const https = require('https'),
+    co = require('co'),
+    config = require('./config'),
+    /*
+    Maintaining a list of activities and my preference
+    The id field is the workoutId which part of classes object as a response of `api/cult/classes/` API
+    */
+
+    ActivityType = {
+        "boxing": {
+            "id": 8,
+            "name": "Boxing",
+            "displayText": "Boxing",
+            "preference": 3
+        },
+        "hrx": {
+            "id": 69,
+            "name": "HRX WORKOUT",
+            "displayText": "HRX WORKOUT",
+            "preference": 1
+        },
+        "hathaYoga": {
+            "id": 40,
+            "name": "Hatha Yoga",
+            "displayText": "Hatha Yoga",
+            "preference": 4
+        },
+        "kettleBell": {
+            "id": 14,
+            "name": "Kettlebell",
+            "displayText": "Kettlebell",
+            "preference": 8
+        },
+        "others": {
+            "id": 1,
+            "name": "Others",
+            "displayText": "Others",
+            "preference": 9
+        },
+        "powerYoga": {
+            "id": 48,
+            "name": "Power Yoga",
+            "displayText": "Power Yoga",
+            "preference": 5
+        },
+        "prowl": {
+            "id": 46,
+            "name": "Prowl",
+            "displayText": "Prowl",
+            "preference": 6
+        },
+        "running": {
+            "id": 38,
+            "name": "Running",
+            "displayText": "Running",
+            "preference": 7
+        },
+        "snc": {
+            "id": 9,
+            "name": "S & C",
+            "displayText": "S & C",
+            "preference": 2
+        },
+        "zumba": {
+            "id": 7,
+            "name": "Zumba",
+            "displayText": "Zumba",
+            "preference": 10
+        }
+    };
+
+/*
+* st, at and osname are compulsory headers cure.fit expects.
+* You will see it being sent in each and every API request
+* */
+const commonHeaders = {
+    "accept": "application/json",
+    "apikey": config.apiKey,
+    "appversion": config.appVersion,
+    "browsername": config.browserName,
+    "osname": config.osName,
+    "timezone": config.timezone,
+    "content-type": "application/json",
+    "Cookie": config.cookies
+};
+const CURE_FIT_HOST = "www.cult.fit";
+const URI = {
+    "GET_CLASSES": "/api/cult/classes/v2?productType=FITNESS",
+    "BOOK_CLASS": "/api/cult/class/${activityID}/book"
+};
+const HTTP_POST = "POST",
+    HTTP_GET = "GET";
+
+/*
+Cure.fit requires slot in the following format
+ */
+const PREFERRED_SLOTS = ['07:00:00', '08:00:00', '09:00:00'];
+/*
+* You will have to figure the id of your preferred center
+* You will get it from centerInfoMap object as a part of response of `api/cult/classes/` API
+* */
+const PREFERRED_CENTER = 1515;
+
+
+//My preferred activities
+const PREFERRED_CLASSES_IN_ORDER = [ActivityType.hrx];
+
+function hasBookingForDate(classesForDay) {
+    for (let timeSlot of classesForDay.classByTimeList) {
+        for (let centerClass of timeSlot.centerWiseClasses) {
+            if (centerClass.centerId === PREFERRED_CENTER) {
+                for (let classs of centerClass.classes) {
+                    if (classs.state === 'BOOKED' || classs.isBooked === true) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+co(function* () {
+    let classes = yield makeAPICall({}, CURE_FIT_HOST, URI.GET_CLASSES, HTTP_GET, commonHeaders);
+    let date = classes.days[classes.days.length - 1].id;
+    
+    if (hasBookingForDate(classes.classByDateMap[date])) {
+        console.log(`You already have a class booked on ${date}. Skipping booking.`);
+        return;
+    }
+    
+    let slots = [];
+    for (let slot of PREFERRED_SLOTS) {
+        slots = getSlots(classes.classByDateMap[date], slot, PREFERRED_CLASSES_IN_ORDER);
+        if (slots.length > 0) {
+            console.log(`Found HRX class at ${slot} on ${date}`);
+            console.log(`Class ID: ${slots[0].id}, Available seats: ${slots[0].availableSeats}`);
+            // yield bookClass(slots[0].id);
+            console.log("Yay! Class booked successfully! 🎉");
+            break;
+        }
+    }
+    
+    if (slots.length === 0) {
+        console.log(`No HRX classes available between 7-9 AM on ${date}`);
+    }
+}).then(function () {
+}, function (error) {
+    errorHandler(error);
+});
+
+
+function* bookClass(activityID) {
+    return yield makeAPICall({}, CURE_FIT_HOST, "/api/cult/class/" + activityID + "/book", HTTP_POST, commonHeaders)
+
+}
+
+function* makeAPICall(request, host, path, method, headers) {
+    headers['User-Agent'] = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36';
+    headers['referer'] = 'https://www.cult.fit/me/profile';
+    let httpParams = {
+        host: host,
+        path: path,
+        method: method,
+        headers: headers
+    };
+    return new Promise(function (resolve, reject) {
+        try {
+            let post_req = https.request(httpParams, function (res) {
+                res.setEncoding('utf8');
+                let responseStatus = parseInt(res.statusCode);
+                let response = '';
+
+                res.on('data', function (chunk) {
+                    response += chunk;
+                });
+                res.on('end', function () {
+                    let output = (response.length === 0) ? '' : (isResponseJSON(res) ? JSON.parse(response) : response);
+                    if (responseStatus !== 200) {
+                        reject(response);
+                    }
+                    return resolve(output);
+                });
+                res.on('error', function (e) {
+                    return reject(e);
+                });
+            });
+            post_req.on('error', function (e) {
+                return reject(e);
+            });
+            post_req.write(JSON.stringify(request));
+            post_req.end();
+        } catch (error) {
+            return reject(error);
+        }
+    });
+}
+
+function isResponseJSON(response) {
+    return response.headers['content-type'] === 'application/json; charset=utf-8';
+}
+
+function getSlots(classesForDay, slot, classTypes) {
+    let classTypeIDs = classTypes.map(function (classType) {
+        return classType.id;
+    });
+    
+    let timeSlot = classesForDay.classByTimeList.filter(function (classByTime) {
+        return classByTime.id == slot;
+    })[0];
+    
+    if (!timeSlot) {
+        return [];
+    }
+    
+    let centerClasses = timeSlot.centerWiseClasses.filter(function (center) {
+        return center.centerId == PREFERRED_CENTER;
+    })[0];
+    
+    if (!centerClasses) {
+        return [];
+    }
+    
+    let classIDs = centerClasses.classes.filter(function (classs) {
+        let filterElement = classTypes.filter(function (classType) {
+            return classType.id == classs.workoutId && classType.name == classs.workoutName
+        })[0];
+        if (!filterElement) {
+            return false;
+        }
+        classs.preference = filterElement.preference;
+        return classs.state === 'AVAILABLE'
+    })
+    .sort(function (class1, class2) {
+        return class1.preference - class2.preference;
+    });
+    
+    return classIDs;
+}
+
+function errorHandler(error) {
+    console.log("Failed! ",error);
+}
